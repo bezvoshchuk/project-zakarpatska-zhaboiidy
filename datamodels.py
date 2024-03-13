66cefa49132cd75ded0d5859c878cf724d04a911
Yana <yanok87@users.noreply.github.com>
2024-03-13 20:13:02 +0200

    Add notes book to datamodel

--- datamodels.py
+++ datamodels.py
@@ -328,7 +328,6 @@ class Note:
         self.name: Name = Name(name_)
         self.project_role: ProjectRole = ProjectRole(project_role)
         self.project_tasks: ProjectTasks = ProjectTasks(project_tasks)
-
         self.hobbies: list[Hobby] = [Hobby(hobby) for hobby in (hobbies or [])]
 
     def add_project_role(self, project_role: str):
@@ -401,3 +400,86 @@ class Note:
 
     def __str__(self):
         return f"Note for: {self.name.value} \n project role: {self.project_role}  \n project tasks: {self.project_tasks} \n hobbies: {'; '.join(h.value for h in self.hobbies)}"
+
+
+class NotesBook(UserDict):
+    data = {}
+
+    def print_notes_book(self):
+        """Print all notes"""
+
+        for name, note in self.data.items():
+            print(note)
+
+    def load_data_from_json(self, json_data):
+        """Print all existing notes from file"""
+
+        self.data = {
+            _note_data["name_"]: Record(**_note_data) for _note_data in json_data
+        }
+
+    def dump_data_to_json(self):
+        """Save notes to file"""
+
+        return [
+            {
+                "name_": _note.name.value,
+                "project_role": str(_note.project_role),
+                "project_tasks": str(_note.project_tasks),
+                "hobbies": [hobby.value for hobby in _note.hobbies],
+            }
+            for _note in self.data.values()
+        ]
+
+    def add_note(self, note_: Note) -> Note | None:
+        """Add a note to notes book if not already present.
+
+        Args:
+            note_: Note to add.
+        """
+        if note_.name.value not in self.data:
+            self.data[note_.name.value] = note_
+            return note_
+
+    def find(self, name_: str) -> Note:
+        """Find a note in the notes book by name.
+
+        Args:
+            name_: Username of the user to find.
+
+        Raises:
+            KeyError: if note doesn't exist.
+        """
+        if name_ not in self.data:
+            raise KeyError(f"Note for {name_} was not found.")
+
+        return self.data[name_]
+
+    def find_project_role(self, project_role_: str) -> Note:
+        """Find a note in the notes book by project role.
+
+        Args:
+            project_role_: Project role of to find.
+
+        Raises:
+            KeyError: if note doesn't exist.
+        """
+        project_role_note = filter(
+            lambda note: note["project_role"] == project_role_, self.data
+        )
+        if project_role_note not in self.data:
+            raise KeyError(f"Note for {project_role_} was not found.")
+
+        return project_role_note
+
+    def delete(self, name_: str) -> None:
+        """Delete a note in the notes book by name.
+
+        Args:
+            name_: Name of the note to find.
+
+        Raises:
+            KeyError: if record doesn't exist.
+        """
+
+        del self.data[name_]
